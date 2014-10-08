7f55f7a8a8a9272bc16bc9d7506624618d6e60d9
Chris AtLee <catlee@mozilla.com>
2014-10-08 14:37:44 -0400

    loggify graphite module

--- reportor/graphite.py
+++ reportor/graphite.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import socket
-import reportor.config
 import time
+import reportor.config
+
+import logging
+log = logging.getLogger(__name__)
+
 
 class GraphiteSubmitter(object):
     def __init__(self, host, port, prefix):
@@ -15,7 +19,7 @@ class GraphiteSubmitter(object):
         if timestamp:
             line += " %i" % timestamp
 
-        #print line
+        log.debug(str(line))
         self._sock.sendall(line + "\n")
 
     def wait(self):
@@ -27,10 +31,10 @@ def graphite_from_config():
     if not config.has_section('graphite'):
         return None
     g = GraphiteSubmitter(
-            host=config.get('graphite', 'hostname'),
-            port=config.getint('graphite', 'port'),
-            prefix=config.get('graphite', 'prefix'),
-            )
+        host=config.get('graphite', 'hostname'),
+        port=config.getint('graphite', 'port'),
+        prefix=config.get('graphite', 'prefix'),
+    )
     # Make sure we sleep to let metrics get through. cf.
     # https://bugzilla.mozilla.org/show_bug.cgi?id=1025145
     time.sleep(1)

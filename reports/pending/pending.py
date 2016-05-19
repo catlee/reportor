c6786c1ef93acc21d34acddf3591611dfcce63dc
Chris AtLee <catlee@mozilla.com>
2016-05-19 10:58:37 -0400

    Add running job data

--- reports/pending/pending.py
+++ reports/pending/pending.py
@@ -4,6 +4,7 @@ from collections import Counter
 import logging
 
 PENDING_URL = "http://buildapi.pvt.build.mozilla.org/buildapi/pending?format=json"
+RUNNING_URL = "http://buildapi.pvt.build.mozilla.org/buildapi/running?format=json"
 ALLTHETHINGS_URL = "https://secure.pub.build.mozilla.org/builddata/reports/allthethings.json"
 
 
@@ -26,13 +27,17 @@ if __name__ == '__main__':
 
     logging.info('Fetching pending info from %s', PENDING_URL)
     pending = requests.get(PENDING_URL).json()
+    logging.info('Fetching running info from %s', RUNNING_URL)
+    running = requests.get(RUNNING_URL).json()
     logging.info('Fetching allthethings from %s', ALLTHETHINGS_URL)
     allthethings = requests.get(ALLTHETHINGS_URL).json()
 
     pending_by_pool = Counter()
+    running_by_pool = Counter()
 
     for pool in allthethings['slavepools'].values():
         pending_by_pool[pool[0].rsplit('-', 1)[0]] = 0
+        running_by_pool[pool[0].rsplit('-', 1)[0]] = 0
 
     for branch, revisions in pending['pending'].items():
         for rev, builds in revisions.items():
@@ -40,10 +45,19 @@ if __name__ == '__main__':
                 buildername = b['buildername']
                 pool = pool_for_builder(allthethings, buildername)
                 pending_by_pool[pool] += 1
-
     print json.dumps(pending_by_pool)
+
+    for branch, revisions in running['running'].items():
+        for rev, builds in revisions.items():
+            for b in builds:
+                buildername = b['buildername']
+                pool = pool_for_builder(allthethings, buildername)
+                running_by_pool[pool] += 1
+    print json.dumps(running_by_pool)
 
     logging.info('Submitting to graphite')
     g = reportor.graphite.graphite_from_config()
     for pool, count in pending_by_pool.items():
         g.submit('releng.pending.{}'.format(pool), count)
+    for pool, count in running_by_pool.items():
+        g.submit('releng.running.{}'.format(pool), count)

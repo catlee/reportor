637371eddb0ca50c2fde0266411caa815002b7e1
Chris AtLee <catlee@mozilla.com>
2017-08-02 08:41:32 -0400

    Fix update sizes report for nightly

--- reports/update_sizes/update_sizes.py
+++ reports/update_sizes/update_sizes.py
@@ -22,7 +22,10 @@ def get_taskId(branch, platform, d):
         product = 'mobile'
     else:
         product = 'firefox'
-    return get_nightly_taskId(branch, platform, d, product)
+    if branch in ('mozilla-central', 'mozilla-aurora'):
+        return get_nightly_taskId(branch, platform, d, product)
+    else:
+        return get_release_taskId(branch, platform, d, product)
 
 
 def get_nightly_taskId(branch, platform, d, product):
@@ -36,7 +39,7 @@ def get_nightly_taskId(branch, platform, d, product):
     return taskId
 
 
-def get_release_taskId(branch, platform, d, product):
+def get_release_taskId(branch, platform, d):
     d = d.strftime('%Y.%m.%d')
     url = 'https://index.taskcluster.net/v1/namespaces/gecko.v2.{branch}.pushdate.{d}'.format(branch=branch, platform=platform, d=d)
     response = requests.post(url, data='{}', headers={'Content-Type': 'application/json'})
@@ -98,26 +101,15 @@ def get_buildprops(taskId):
         return resp.json()
 
 
-def get_targetjson(taskId):
-    url = get_artifact_url(taskId, 'public/build/target.json')
-    resp = requests.get(url)
-    if resp.status_code == 200:
-        return resp.json()
-
-
 def get_artifact_url(taskId, artifact):
     return 'https://queue.taskcluster.net/v1/task/{taskId}/artifacts/{artifact}'.format(taskId=taskId, artifact=artifact)
 
 
 def get_partial_url(branch, platform, version, buildid, lastbuildid):
-    if branch == 'mozilla-central':
-        return 'https://s3.amazonaws.com/mozilla-nightly-updates/{branch}/{buildid}/Firefox-{branch}-{version}-{platform}-en-US-{lastbuildid}-{buildid}.partial.mar'.format(branch=branch, platform=platform, buildid=buildid, lastbuildid=lastbuildid, version=version)
-    else:
-        # http://releases.mozilla.com/pub/firefox/releases/55.0b1/update/win64/en-US/firefox-54.0-55.0b1.partial.mar
-        return 'https://releases.mozilla.com/pub/firefox/releases/{version}/update/{platform}/en-US/firefox-{lastversion}-{version}.partial.mar'.format(version=version, platform=platform, lastversion=lastbuildid)
+    return 'https://s3.amazonaws.com/mozilla-nightly-updates/{branch}/{buildid}/Firefox-{branch}-{version}-{platform}-en-US-{lastbuildid}-{buildid}.partial.mar'.format(branch=branch, platform=platform, buildid=buildid, lastbuildid=lastbuildid, version=version)
 
 
-def get_nightly_sizes(branch, p, d, lastbuildid):
+def get_sizes(branch, p, d, lastbuildid):
     taskId = get_taskId(branch, p, d)
     if not taskId:
         print 'Couldn\'t find task for', branch, p, d
@@ -137,15 +129,10 @@ def get_nightly_sizes(branch, p, d, lastbuildid):
         complete_size = None
 
     buildprops = get_buildprops(taskId)
-    targetjson = get_targetjson(taskId)
     if buildprops:
         buildid = buildprops.get('properties', {}).get('buildid')
         version = buildprops.get('properties', {}).get('appVersion')
-    elif targetjson:
-        buildid = targetjson.get('buildid')
-        version = targetjson.get('moz_app_version')
     else:
-        print 'Couldn\'t find buildprops for', taskId
         buildid = None
         version = None
 
@@ -159,7 +146,6 @@ def get_nightly_sizes(branch, p, d, lastbuildid):
     else:
         partial_size = None
 
-
     return dict(
         installer_size=installer_size,
         complete_size=complete_size,
@@ -168,12 +154,11 @@ def get_nightly_sizes(branch, p, d, lastbuildid):
 
 
 def main():
-    #import reportor.graphite
-    #graphite = reportor.graphite.graphite_from_config()
+    import reportor.graphite
+    graphite = reportor.graphite.graphite_from_config()
 
     platforms = ['linux64', 'win32', 'win64', 'macosx64', 'android-api-15', 'android-x86']
-    branches = ['mozilla-central', 'mozilla-beta', 'mozilla-release']
-    branches = ['mozilla-beta', 'mozilla-release']
+    branches = ['mozilla-central']
 
     oneday = timedelta(days=1)
     today = date.today()
@@ -181,29 +166,24 @@ def main():
     days = 5
 
     for branch in branches:
-        if branch == 'mozilla-central':
-            for p in platforms:
-                d = today
-                taskId = None
-                lastbuildid = None
-                for i in range(days):
-                    d = today - i * oneday
-                    sizes = get_nightly_sizes(branch, p, d, lastbuildid)
-                    if not sizes:
-                        continue
-                    #graphite.submit('release_sizes.{}.{}.installer'.format(branch, p), sizes['installer_size'], date2ts(d))
-                    #if sizes['complete_size']:
-                        #graphite.submit('release_sizes.{}.{}.complete'.format(branch, p), sizes['complete_size'], date2ts(d))
-                    #if sizes['partial_size']:
-                        #graphite.submit('release_sizes.{}.{}.partial'.format(branch, p), sizes['partial_size'], date2ts(d))
+        for p in platforms:
+            d = today
+            taskId = None
+            lastbuildid = None
+            for i in range(days):
+                d = today - i * oneday
+                sizes = get_sizes(branch, p, d, lastbuildid)
+                if not sizes:
+                    continue
+                graphite.submit('release_sizes.{}.{}.installer'.format(branch, p), sizes['installer_size'], date2ts(d))
+                if sizes['complete_size']:
+                    graphite.submit('release_sizes.{}.{}.complete'.format(branch, p), sizes['complete_size'], date2ts(d))
+                if sizes['partial_size']:
+                    graphite.submit('release_sizes.{}.{}.partial'.format(branch, p), sizes['partial_size'], date2ts(d))
 
-                    lastbuildid = sizes['buildid']
-                    print d, branch, p, sizes
-        else:
-            # Figure out something for releases...
-            # Partials can be found at e.g. 
-            # https://tools.taskcluster.net/index/artifacts/#releases.v1.mozilla-beta.1b954c82dd04faf1926804d89c0d130dc6b9ab93.firefox.51_0b4.build1.partials.51_0b3.win64/releases.v1.mozilla-beta.1b954c82dd04faf1926804d89c0d130dc6b9ab93.firefox.51_0b4.build1.partials.51_0b3.win64.en-US
-            # How to figure out the previous versions?
+                lastbuildid = sizes['buildid']
+                print d, branch, p, sizes
+
 
 if __name__ == '__main__':
     main()
